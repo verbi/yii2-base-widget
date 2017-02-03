7bad1e849eae8ab8e0944983681762b474c7ccc6
Philip Verbist <philip.verbist@gmail.com>
2017-02-03 02:06:37 +0100

    Initial commit

---
+++ src/widgets/Widget.php
@@ -0,0 +1,10 @@
+<?php
+namespace verbi\yii2BaseWidget\widgets;
+/*
+ * @author Philip Verbist <philip.verbist@gmail.com>
+ * @link https://github.com/verbi/Yii2-Helpers/
+ * @license https://opensource.org/licenses/GPL-3.0
+*/
+class Widget extends \yii\base\Widget {
+    
+}
